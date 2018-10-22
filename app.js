f59b957362c5a0c53850f086fa5defc0df2d720f
Joe Thaxton <14jthaxton@gmail.com>
2018-10-21 22:32:05 -0700

    Auth push

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const users = require("./routes/api/users");
+const events = require("./routes/api/events");
+const register = require("./routes/api/register");
+const passport = require('passport');
+require("./config/passport")(passport);
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+
+app.use("/api/users", users);
+app.use("/api/events", events);
+app.use("/api/register", register);
+
+app.use(bodyParser.json());
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb://Joe:Jt92106..@ds137483.mlab.com:37483/borednomore",
+    secretOrKey: 'secret'
+
+};
