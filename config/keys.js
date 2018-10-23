171d028796ddce741927b50adc9b1779c2b91cbe
tonyjhuang <huangtonyj91@gmail.com>
2018-10-23 14:33:31 -0700

    update gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: "mongodb://Joe:Jt92106..@ds137483.mlab.com:37483/borednomore",
-    secretOrKey: 'secret'
-
+    secretOrKey: 'secret',
+    apiKeyYelp: 'poC8AfjbLYauoNBsYcAV6BuMG_R0xy9xYYO4sJBYK4TB1f_g3tLjvLSHRDSf5hDtmauRHqydPBLYwy-_oo_1tDRC2xXvVJcAuQlb4xivd5f9S2lakMItDP_30w3OW3Yx',
 };
