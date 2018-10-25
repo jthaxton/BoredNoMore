69cb41c2e678120aeb2fc496612e1f526ac44547
tonyjhuang <huangtonyj91@gmail.com>
2018-10-25 13:35:30 -0700

    Add more input parameters to api query

--- frontend/src/util/api_ticket_master.js
+++ frontend/src/util/api_ticket_master.js
@@ -3,59 +3,82 @@ const Geohash = require('latlon-geohash');
 const apiKeyTicketMaster = require("../../../config/keys").apiKeyTicketMaster;
 const apiSecretTicketMaster = require("../../../config/keys").apiSecretTicketMaster;
 
-// export const fetchTicketMaster = () => (
+// export const fetchTicketMaster = (options) => (
 const fetchTicketMaster = (options) => {
+  // Default query parameters
   const defaults = {
     latitude: 37.7986717,
     longitude: -122.4012891, // San Francisco, CA
+
+    includeFamily: 'no',
+
     radius: 50,
     size: 20,
   };
 
+  // Combine defaults and options query parameters
   const {
-    startDateTime,
-    endDateTime,
-    geoPoint,
+    date,
+
     latitude,
     longitude,
-
     radius,
+
+    includeFamily,
+
     size,
+    page,
   } = Object.assign(defaults, options);
 
+  // Generate DateTime based on date options input
+  const startDateTime = `${date}T00:00:00Z`;
+  const endDateTime = `${date}T23:59:59Z`;
+  const onsaleEndDateTime = `${date}T23:59:59Z`;
+
+  // Generate geoPoint parameter based on latitude, longitude
+  const geoPoint = Geohash.encode(latitude, longitude, 3);
+
+  // Generate URL path
   let url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
 
   url += startDateTime ? `&startDateTime=${startDateTime}` : ''
   url += endDateTime ? `&endDateTime=${endDateTime}` : ''
-  url += geoPoint ? `&geoPoint=${geoPoint}` : ''
+  url += onsaleEndDateTime ? `&onsaleEndDateTime=${onsaleEndDateTime}` : ''
 
+  url += geoPoint ? `&geoPoint=${geoPoint}` : ''
   url += radius ? `&radius=${radius}` : ''
+
+  url += includeFamily ? `&includeFamily=${includeFamily}` : ''
+
   url += size ? `&size=${size}` : ''
+  url += page ? `&page=${page}` : ''
 
   url += `&apikey=${apiKeyTicketMaster}`
 
+  console.log(url)
 
+  // Return axios Promise
   return axios({
     method: 'get',
     url: url,
     responseType: 'json'
   })
-
-  console.log(Geohash.encode(latitude, longitude, 3));
 }
 
 // fetchTicketMaster()
 fetchTicketMaster({
-  geoPoint: 'drt2zp',
+    latitude: 42.35984802,
+    longitude: -71.05888367,
 
-  latitude: 37.7986717,
-  longitude: -122.4012891,
+    date: '2018-11-30',
 
-  startDateTime: '2018-10-27T00:00:00Z',
-  endDateTime: '2018-10-27T23:59:59Z',
-  size: 1,
-})
-// .then((res) => {
-//   console.log(res.data._embedded.events)
-//   console.log(res.data._embedded.events[0].dates)
-// })
+    includeFamily: 'no', // family-friendly?
+
+    size: 20, // how many query results to return
+    page: 0, // aka offset
+  })
+  .then((res) => {
+    console.log(res.data._embedded.events) // Array of events
+    // console.log(res.data._embedded.events[0]._embedded.venues) // Single event's venue
+    // console.log(res.data._embedded.events[0].dates) // Single event's date
+  })
