052c982fc8763607d07b40e75532e82328c7cbeb
tonyjhuang <huangtonyj91@gmail.com>
2018-10-25 11:25:15 -0700

    Refactor TicketMaster API

--- frontend/src/util/api_ticket_master.js
+++ frontend/src/util/api_ticket_master.js
@@ -1,35 +1,61 @@
 const axios = require('axios');
+const Geohash = require('latlon-geohash');
 const apiKeyTicketMaster = require("../../../config/keys").apiKeyTicketMaster;
 const apiSecretTicketMaster = require("../../../config/keys").apiSecretTicketMaster;
 
-// // export const fetchTicketMaster = () => (
-// const fetchTicketMaster = () => (
-//   $.ajax({
-//     method: 'GET',
-//     url: "https://app.ticketmaster.com/discovery/v2/events.json?size=2&geoPoint=txd8bzr&apikey=elVpo7apIVQYzAZxikTRVLfd43PHzikt",
-//     async: true,
-//     dataType: "json",
-//   })
-// );
+// export const fetchTicketMaster = () => (
+const fetchTicketMaster = (options) => {
+  const defaults = {
+    latitude: 37.7986717,
+    longitude: -122.4012891, // San Francisco, CA
+    radius: 50,
+    size: 20,
+  };
 
-// fetchTicketMaster()
-// fetchTicketMaster().then((res) => console.log(res), (res) => console.log(res));
+  const {
+    startDateTime,
+    endDateTime,
+    geoPoint,
+    latitude,
+    longitude,
+
+    radius,
+    size,
+  } = Object.assign(defaults, options);
+
+  let url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+
+  url += startDateTime ? `&startDateTime=${startDateTime}` : ''
+  url += endDateTime ? `&endDateTime=${endDateTime}` : ''
+  url += geoPoint ? `&geoPoint=${geoPoint}` : ''
+
+  url += radius ? `&radius=${radius}` : ''
+  url += size ? `&size=${size}` : ''
+
+  url += `&apikey=${apiKeyTicketMaster}`
 
 
-const size = 1;
-const geoPoint = '9q8zn9n';
-
-let url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
-url += size ? `&size=${size}` : ''
-url += geoPoint ? `&geoPoint=${geoPoint}` : ''
-url += `&apikey=${apiKeyTicketMaster}`
-
-axios({
+  return axios({
     method: 'get',
     url: url,
     responseType: 'json'
   })
-  .then((res) => {
-    console.log(res.data._embedded.events)
-    console.log(res.data._embedded.events[0].dates)
-  })
+
+  console.log(Geohash.encode(latitude, longitude, 3));
+}
+
+// fetchTicketMaster()
+fetchTicketMaster({
+  geoPoint: 'drt2zp',
+
+  latitude: 37.7986717,
+  longitude: -122.4012891,
+
+  startDateTime: '2018-10-27T00:00:00Z',
+  endDateTime: '2018-10-27T23:59:59Z',
+  size: 1,
+})
+// .then((res) => {
+//   console.log(res.data._embedded.events)
+//   console.log(res.data._embedded.events[0].dates)
+// })
