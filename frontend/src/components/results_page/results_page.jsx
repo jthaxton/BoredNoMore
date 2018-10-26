04b4f6f9a185a1655c4028cc0f9923b6410293dd
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-25 21:15:51 -0700

    Style results page

--- frontend/src/components/results_page/results_page.jsx
+++ frontend/src/components/results_page/results_page.jsx
@@ -17,8 +17,10 @@ export default class resultsPage extends Component {
             <section className="user-greeting">
               {/* Welcome, {this.props.currentUser.name} */}
                 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous"/>
-              <i className="fas fa-user-alt" />
-              Welcome, User
+              <p className="welcomeuser">
+                <i className="fas fa-user-alt" />
+                Welcome, User
+              </p>
             </section>
             <h1 className="borednomore-results">BoredNoMore</h1>
           </div>
@@ -41,26 +43,33 @@ export default class resultsPage extends Component {
             <ul className="results">
               <li className="streamSelection">
                 {/* onClick ={() => <Modal selection="stream"/>}> */}
-                <div className="image-stand-in">stand-in for image</div>
-                {/* <img src="some url that we will likely get from props" /> */}
+                {/* <div className="image-stand-in">stand-in for image</div> */}
+                <img src="https://images.unsplash.com/photo-1521967906867-14ec9d64bee8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=63f399f203a46024cdee72cd6aa42163&auto=format&fit=crop&w=1350&q=80" />
                 {/* <h4>{this.props.streamSelection}</h4> */}
+                <p>Streaming Selection Placeholder - Stream Source</p>
               </li>
 
               <li className="restaurantSelection">
-                <div className="image-stand-in">stand-in for image</div>                {/* <img src="some url that we will likely get from props" /> */}
+                {/* <div className="image-stand-in">stand-in for image</div>                <img src="some url that we will likely get from props" /> */}
+                <img src="https://images.unsplash.com/photo-1527224538127-2104bb71c51b?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c7fc4917011de5709727efa4b8497bad&auto=format&fit=crop&w=1351&q=80" />
                 {/* <h4>{this.props.restaurantSelection}</h4> */}
+                <p>Restaurant Selection Placeholder - San Francisco, CA</p>
               </li>
 
               <li className="movieSelection">
-                <div className="image-stand-in">stand-in for image</div>
-                {/* <img src="some url that we will likely get from props" /> */}
+                {/* <div className="image-stand-in">stand-in for image</div> */}
+                <img src="https://images.unsplash.com/photo-1513106580091-1d82408b8cd6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c93960a66ab95463358588dd85dc9d26&auto=format&fit=crop&w=1355&q=80" />
                 {/* <h4>{this.props.movieSelection}</h4> */}
+                <p>Movie Selection Placeholder - San Francisco, CA</p>
               </li>
 
               <li className="eventSelection">
-                <div className="image-stand-in">stand-in for image</div>
+                {/* <div className="image-stand-in">stand-in for image</div> */}
                 {/* <img src="some url that we will likely get from props" /> */}
+                <img src="https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3a413a0b054159dd7840130c25e6dbdf&auto=format&fit=crop&w=1350&q=80" />
                 {/* <h4>{this.props.eventSelection}</h4> */}
+                <p>Event Selection Placeholder - San Francisco, CA</p>
+
               </li>
             </ul>
           </div>
