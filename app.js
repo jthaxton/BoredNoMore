24437decc6f9f7a5f545448bc5e4dcca759e1aab
Joe Thaxton <14jthaxton@gmail.com>
2018-10-26 13:59:30 -0700

    heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const bodyParser = require("body-parser");
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("frontend/build"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
 }
 
