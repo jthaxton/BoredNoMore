d7523e25eb73bf7f2657c1697d34ed439d5f2a66
Chris Atwood <coachchris289@gmail.com>
2018-10-27 15:47:49 -0700

    add images to whyUs

--- frontend/src/components/whyUs/whyUs.jsx
+++ frontend/src/components/whyUs/whyUs.jsx
@@ -6,20 +6,27 @@ export default class whyUs extends React.Component {
         return(
         <div className="whyUs">
             <ul className="whyUs-list">
-
-                <li className="whyUs-idx-1" id="whyUs-start"> 
-                    <div className="whyUs-content" >
-                        <img src="https://cdn0.iconfinder.com/data/icons/geosm-e-commerce/48/info-512.png" />
-                        <h3>5 WAYS WE MAKE THE BEST SUGGESTIONS </h3>
+             
+                <li className="whyUs-idx-1" id="whyUs-splash"> 
+                    <div className="whyUs-content">
+                        <div className="whyUs-list-text" id="whyUs-splash-text">
+                            <img src="https://cdn2.iconfinder.com/data/icons/circle-icons-1/64/bar-chart-512.png" />
+                            <h3> THE 4 STEPS TO PERFECT SUGGETIONS </h3>
+                        </div>
                     </div>
-                </li>                
+                </li>
+
+
+
 
                 <li className="whyUs-idx-2"> 
                     <div className="whyUs-content">
                     <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-one-512.png" />
                         <div className="whyUs-list-text">
                             <h3> ITS ALL ABOUT YOU </h3>
-                            <p>We get all of the things you already like to do and watch</p>
+                            <p>We get a list of all of your favorite activities and choices so we can 
+                                taylor our recommendations around what you like.
+                            </p>
                         </div>
                     <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
 
@@ -30,9 +37,11 @@ export default class whyUs extends React.Component {
                     <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-two-512.png" />
                         <div className="whyUs-list-text">
                         <h3> WE FIND ALL OF THE POSSIBILITIES </h3>
-                        <p>We scour the internet for everything you could possibly consider doing</p>
+                        <p>We scour the internet for everything you could possibly do,
+                            so that we have all the options when choosing what to recommend.
+                        </p>
                         </div>
-                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+                    <img src="https://cdn1.iconfinder.com/data/icons/the-movies/100/movie-06-512.png" />
 
                     </div>
                 </li>
@@ -40,11 +49,12 @@ export default class whyUs extends React.Component {
                     <div className="whyUs-content">
                     <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-three-512.png" />
                         <div className="whyUs-list-text">
-                    <h3> GET INFORMED RECOMMENDATIONS </h3>
-                    <p>We look at a wide range of paramaters of what's popular, what's 
-                        trending, and what's new</p>
+                    <h3> INFORMED RECOMMENDATIONS </h3>
+                    <p>We look at a wide range of paramaters including what's 
+                        trending, and what's new to feed our recommendations algorithem.  Then we compare
+                        that to what you've told us you love.</p>
                         </div>
-                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+                    <img src="https://cdn1.iconfinder.com/data/icons/infographic-vol-7/512/10-128.png" />
 
                     </div>
                 </li>
@@ -57,7 +67,7 @@ export default class whyUs extends React.Component {
                         and the accuracy of our suggestions improves over time.
                     </p>
                         </div>
-                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+                    <img src="https://cdn0.iconfinder.com/data/icons/tutor-icon-set/512/diploma_icon-512.png" />
 
                     </div>
                 </li>
