f82bc5ba819909e6fae98fa4f580b1df574222e6
Chris Atwood <coachchris289@gmail.com>
2018-10-27 14:20:22 -0700

    add basic content to whyUs

--- frontend/src/components/whyUs/whyUs.jsx
+++ frontend/src/components/whyUs/whyUs.jsx
@@ -6,29 +6,63 @@ export default class whyUs extends React.Component {
         return(
         <div className="whyUs">
             <ul className="whyUs-list">
-                <li className="whyUs-idx-1"> 
-                    <div className="whyUs-content">
+
+                <li className="whyUs-idx-1" id="whyUs-start"> 
+                    <div className="whyUs-content" >
                         <img src="https://cdn0.iconfinder.com/data/icons/geosm-e-commerce/48/info-512.png" />
-                        <p>How do we find the best suggestions? </p>
+                        <h3>5 WAYS WE MAKE THE BEST SUGGESTIONS </h3>
                     </div>
                 </li>                
 
                 <li className="whyUs-idx-2"> 
-                    <p>movies</p> 
+                    <div className="whyUs-content">
+                    <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-one-512.png" />
+                        <div className="whyUs-list-text">
+                            <h3> ITS ALL ABOUT YOU </h3>
+                            <p>We get all of the things you already like to do and watch</p>
+                        </div>
+                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
 
+                    </div>
                 </li>
-
                 <li className="whyUs-idx-3"> 
-                    restaurants
+                    <div className="whyUs-content">
+                    <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-two-512.png" />
+                        <div className="whyUs-list-text">
+                        <h3> WE FIND ALL OF THE POSSIBILITIES </h3>
+                        <p>We scour the internet for everything you could possibly consider doing</p>
+                        </div>
+                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+
+                    </div>
                 </li>
-                
                 <li className="whyUs-idx-4"> 
-                    events
+                    <div className="whyUs-content">
+                    <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-three-512.png" />
+                        <div className="whyUs-list-text">
+                    <h3> GET INFORMED RECOMMENDATIONS </h3>
+                    <p>We look at a wide range of paramaters of what's popular, what's 
+                        trending, and what's new</p>
+                        </div>
+                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+
+                    </div>
+                </li>
+                <li className="whyUs-idx-5"> 
+                    <div className="whyUs-content">
+                    <img src="https://cdn2.iconfinder.com/data/icons/integers/60/number-four-512.png" />
+                        <div className="whyUs-list-text">
+                    <h3> WE LEARN </h3>
+                    <p>As you make choices, our algorithem learns over time what you like
+                        and the accuracy of our suggestions improves over time.
+                    </p>
+                        </div>
+                    <img src="https://cdn3.iconfinder.com/data/icons/higher-education-icon-set/256/idea.png" />
+
+                    </div>
                 </li>
                 
-                <li className="whyUs-idx-5"> 
-                    concerts
-                </li>
+
             </ul>
 
         </div>
