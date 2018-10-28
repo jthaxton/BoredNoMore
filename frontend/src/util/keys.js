dc844ce3472e5d3e17640321cdfa8e956dfdec18
huangtonyj <33407842+huangtonyj@users.noreply.github.com>
2018-10-27 17:50:49 -0700

    Delete keys.js

--- frontend/src/util/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://Joe:Jt92106..@ds137483.mlab.com:37483/borednomore",
-  secretOrKey: 'secret',
-  apiKeyYelp: 'poC8AfjbLYauoNBsYcAV6BuMG_R0xy9xYYO4sJBYK4TB1f_g3tLjvLSHRDSf5hDtmauRHqydPBLYwy-_oo_1tDRC2xXvVJcAuQlb4xivd5f9S2lakMItDP_30w3OW3Yx',
-  apiKeyFandango: '6e92dttf4tpwfejpryv4m55r',
-  apiSecretFandango: '6vhkJk9ZE9'
-};
