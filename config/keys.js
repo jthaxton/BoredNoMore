56b524ac417609f7943e4f2b21d58a19b717c048
tonyjhuang <huangtonyj91@gmail.com>
2018-10-28 17:04:47 -0700

    fix bug and update gitignore and keys

--- config/keys.js
+++ config/keys.js
@@ -3,7 +3,12 @@ let keys = {};
 if (process.env.NODE_ENV === "production") {
   keys = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+    secretOrKey: process.env.SECRET_OR_KEY,
+    apiKeyYelp: process.env.API_KEY_YELP,
+    apiKeyFandango: process.env.API_KEY_FANDANGO,
+    apiSecretFandango: process.env.SECRET_FANDANGO,
+    apiKeyTicketMaster: process.env.API_KEY_TICKETMASTER,
+    apiSecretTicketMaster: process.env.SECRET_TICKETMASTER
   };
 } else {
   keys = require("./keys_dev");
