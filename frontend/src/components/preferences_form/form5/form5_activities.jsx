20f38840c91b8a63f1ab5644b42501eb755faddd
Stefanie Luu <luu.stefanie@gmail.com>
2018-10-29 00:41:49 -0700

    fix events skiiresort/tours type as array

--- frontend/src/components/preferences_form/form5/form5_activities.jsx
+++ frontend/src/components/preferences_form/form5/form5_activities.jsx
@@ -11,14 +11,16 @@ export default class Form5Activities extends Component {
     handleSelect(activity) {
         return (e) => {
             let activitiesArr = []
-
             if (activitiesArr.length > 3) {
                 alert("You may only select 3 genres.");
             } else {
-                activitiesArr = this.state.activities;
-                if (!activitiesArr.includes(activity)) {
-                    activitiesArr.push(activity);
-                    this.setState({ activities: activitiesArr });
+                for(let i = 0; i < activity.length; i++){
+                    activitiesArr = this.state.activities;
+                    if (!activitiesArr.includes(activity[i])) {
+                        activitiesArr.push(activity[i]);
+                        this.setState({ activities: activitiesArr });
+                    }
+                    
                 }
             }
         }
@@ -42,27 +44,27 @@ export default class Form5Activities extends Component {
                     <h1>What activities do you enjoy?</h1>
               <ul className="form-list">
                         <li>
-                            <button onClick={this.handleSelect("active")}> ACTIVE </button >
+                            <button onClick={this.handleSelect(["active"])}> ACTIVE </button >
                         </li>
 
                         <li>
-                            <button onClick={this.handleSelect("arts")} > ARTS </button >
+                            <button onClick={this.handleSelect(["arts"])} > ARTS </button >
                         </li>
 
                         <li>
-                            <button onClick={this.handleSelect("beautysvc")} > BEAUTY </button >
+                            <button onClick={this.handleSelect(["beautysvc"])} > BEAUTY </button >
                         </li>
 
                         <li>
-                            <button onClick={this.handleSelect("localservices")} > SERVICES </button >
+                            <button onClick={this.handleSelect(["localservices"])} > SERVICES </button >
                         </li>
 
                         <li>
-                            <button onClick={this.handleSelect("shopping")} > SHOPPING </button >
+                            <button onClick={this.handleSelect(["shopping"])} > SHOPPING </button >
                         </li>
 
                         <li>
-                            <button onClick={this.handleSelect("skiresorts")} > SKIING </button >
+                            <button onClick={this.handleSelect(["skiresorts", "tours"])} > SKIING/TOURS </button >
                         </li>
                     </ul>
                 </form>
